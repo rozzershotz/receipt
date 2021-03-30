eefefeb5fc1174fa8693de8cc1536f8494d59321
Rt@ylor1928 <q13rtaylor@wgsf.net>
2021-03-30 20:12:37 +0100

    commit to git

--- codew.py
+++ codew.py
@@ -12,11 +12,13 @@ basket1 = [
 ["Breakfast cereal",1.49,0]
   ]
 
+# making the quit screen
 def QuitScreen():
   os.system("cls")
   print("BYE")
   time.sleep(0.5)
 
+# making the menu
 def Menu(options, acceptableMenuChoices):
   print("")
   print("Please choose your option: ")
@@ -29,62 +31,69 @@ def Menu(options, acceptableMenuChoices):
     theChoice = input("Choose ur thing idk: ").lower()
   return theChoice
 
+# making the receipt
 def Receipt():
   os.system("cls")
   TotalPrice = 0.0
   TotalDiscount = 0.0
 
+  # carrier bag option
+  bagCost = 0.05
+  carrierBag = input("Do you want a carrier bag? Y/N:")
+  if carrierBag == "Y":
+    basket1.append(["carrier bag",0.05,0])
+
 # receipt layout
-  print("**************************************************")
+  os.system("cls")
+  print("*" * 35)
   print("")
   print("Main Street Supermarket")
   print("")
   for item in basket1:
       price = item[1]
+      # aligning the price and the item
+      lenn = len(item[0])
+      spaces = 30 - lenn
+      
       #working out the discount and taking it away from the total cost
       if item[2] > 0:
           discount = (item[1] / 100) * item[2]
           price = round(item[1] - discount, 2)
           TotalDiscount = TotalDiscount + discount
           roundedDiscount = round(TotalDiscount, 2)
-      print(str(item[0]) + " - " + "£" + str(price))
+      print(str(item[0]) + " " * spaces + "£" + str(price))
       TotalPrice = TotalPrice + price
       RoundedTotal = round(TotalPrice, 2)
   # getting the date and time
   datetime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
   print("")
-  print("------------")
+  print("-" * 12)
   print("")
   # printing total and money saved
-  print("The total is: £" + str(RoundedTotal))
-  print("total money saved from discounts: £" + str(roundedDiscount))
+  print("The total is:" + " " * 17 + "£" + str(RoundedTotal))
+  print("you have saved:" + " " * 15 + "£" + str(roundedDiscount))
   print("")
   #idk lol
-  print("Cat Points earned: 19")
+  print("Cat Points earned:                9")
 
   print("")
-  print("------------")
+  print("-" * 12)
   print("")
   # thankyou message
   print("Thankyou for shopping with us!")
   print("")
   print(datetime)
-  print("**************************************************")
+  print("*" * 35) 
   print("")
-  # carrier bag option
-  bagCost = 0.05
-  carrierBag = input("Do you want a carrier bag? Y/N:")
-  if carrierBag == "Y":
-    NewTotal = (RoundedTotal + bagCost)
-    print("They cost 5p, so your total is now " + str(NewTotal))
-  else:
-    # kind of quit screen
-    time.sleep(0.5)
-    print("")
-    option = input("press 'q' to quit: ")
-    if option == "q":
-      QuitScreen()
+  
+  # kind of quit screen
+  time.sleep(0.5)
+  print("")
+  option = input("press 'q' to quit: ")
+  if option == "q":
+    QuitScreen()
 
+# start menu
 menu = Menu([
   "r - reciept",
   "q - quit"
